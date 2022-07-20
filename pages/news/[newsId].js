11e70d6852b67a719b2ad5d90364b40437563f89
asima-chowdhury <asimachowdhury.lu@gmail.com>
2022-07-20 16:45:51 +0600

    linking between pages

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,6 +1,18 @@
+import { useRouter } from 'next/router'
+
 // our-domain.com/news/something-important
 
 function DeatilPage(){
+    const router = useRouter()
+
+    // console.log(router.query.newsId);
+
+    const newsid = router.query.newsId;
+
+    // Extracting Dynamic Route Data
+    // send a request to the backend API
+    // to fetch the news item with newsId
+
     return <h1>Hi from Deatils Page</h1>
 }
 
--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,25 @@
 // our-domain.com/news
+import Link from 'next/link';
+import {Fragment} from 'react';
 
 function NewsPage(){
-    return <h1>Hi from News Page</h1>
+    return (
+        <Fragment>
+            <h1>Hi from News Page</h1>
+            <ul>
+                <li>
+                    <Link href="/news/nextjs-is-a-great-framework">
+                        Next js is a great framework
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/news/oo">
+                        Something else
+                    </Link>
+                </li>
+            </ul>
+        </Fragment>
+    )
 }
 
 export default NewsPage;
